025b5dd7adf6f2922a28a361681a801b707b531f
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2024-04-13 15:52:51 +0200

    Update Libro Getters And toString

--- src/bibilioteca/dominio/Libro.java
+++ src/bibilioteca/dominio/Libro.java
@@ -15,6 +15,22 @@ public class Libro {
         this.estado_prestamo = false;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getAño_publicacion() {
+        return año_publicacion;
+    }
+
     public boolean estaPrestado() {
         return estado_prestamo;
     }
@@ -27,4 +43,14 @@ public class Libro {
         this.estado_prestamo = false;
     }
 
+    @Override
+    public String toString() {
+        String libro = "Información del libro:\n";
+        libro += "- Título: " + getTitulo() + "\n";
+        libro += "- Autor: " + getAutor() + "\n";
+        libro += "- Género: " + getGenero() + "\n";
+        libro += "- Año Publicación: " + getAño_publicacion() + "\n";
+        libro += "- Prestado: " + estaPrestado() + "\n";
+        return libro;
+    }
 }
