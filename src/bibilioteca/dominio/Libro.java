ae160f4d3a9eee473a1d72be510cff3643f49dc6
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2024-04-13 15:37:30 +0200

    Update Libro estado_prestamo

--- src/bibilioteca/dominio/Libro.java
+++ src/bibilioteca/dominio/Libro.java
@@ -7,12 +7,24 @@ public class Libro {
     private final int año_publicacion;
     private boolean estado_prestamo;
 
-    public Libro(String titulo, String autor, String genero, int año_publicacion){
+    public Libro(String titulo, String autor, String genero, int año_publicacion) {
         this.titulo = titulo;
         this.autor = autor;
         this.genero = genero;
         this.año_publicacion = año_publicacion;
         this.estado_prestamo = false;
     }
-    
+
+    public boolean estaPrestado() {
+        return estado_prestamo;
+    }
+
+    public void prestarLibro() {
+        this.estado_prestamo = true;
+    }
+
+    public void devolverLibro() {
+        this.estado_prestamo = false;
+    }
+
 }
