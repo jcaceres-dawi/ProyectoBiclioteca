b9fe81d1bed5b8c64bb95937d8171c44395cd057
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2024-04-14 10:42:31 +0200

    Update Main With Prestamo

--- src/bibilioteca/main/Main.java
+++ src/bibilioteca/main/Main.java
@@ -1,6 +1,7 @@
 package bibilioteca.main;
 
 import bibilioteca.dominio.Libro;
+import bibilioteca.dominio.Prestamo;
 import bibilioteca.dominio.Usuario;
 
 public class Main {
@@ -8,13 +9,23 @@ public class Main {
         Libro ImperioFinal = new Libro("El Imperio Final", "Brandon Sanderson", "Fantasía", 2006);
         Libro PozoAscension = new Libro("EL Pozo De La Ascensión ", "Brandon Sanderson", "Fantasía", 2007);
 
-     /*    System.out.println(ImperioFinal.toString());
-        System.out.println(PozoAscension.toString()); */
+        /*
+         * System.out.println(ImperioFinal.toString());
+         * System.out.println(PozoAscension.toString());
+         */
 
         Usuario Juan = new Usuario("Juan", "12345678A", "123 45 67 89");
-       /*  System.out.println(Juan.toString()); */
+        /* System.out.println(Juan.toString()); */
 
         Juan.prestarLibro(ImperioFinal);
         System.out.println(Juan.getLibros());
+
+        Prestamo P1 = new Prestamo(ImperioFinal, Juan, "14/04/2024", "14/05/2024");
+        System.out.println(P1.getFinal_prestamo());
+
+        P1.actualizarFinal_prestamo("31/05/2024");
+        System.out.println(P1.getFinal_prestamo());
+
+        System.out.println(P1.toString());
     }
 }
