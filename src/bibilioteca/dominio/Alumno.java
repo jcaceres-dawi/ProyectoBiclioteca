977b56d23f1aec98d5397fce71620e38317d7e4d
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2024-04-21 10:31:52 +0200

    Update Alumno and Profesor

--- src/bibilioteca/dominio/Alumno.java
+++ src/bibilioteca/dominio/Alumno.java
@@ -1,8 +1,17 @@
 package bibilioteca.dominio;
 
 public class Alumno extends Usuario {
-    
-    public Alumno(String nombre, String dni, String telefono){
-        super(nombre, dni, telefono, 10);
+
+    private final int membresia;
+
+    public Alumno(String nombre, String dni, String telefono) {
+        super(nombre, dni, telefono);
+        this.membresia = 10;
     }
+
+    @Override
+    public int getMembresia() {
+        return membresia;
+    }
+
 }
--- src/bibilioteca/dominio/Profesor.java
+++ src/bibilioteca/dominio/Profesor.java
@@ -3,6 +3,11 @@ package bibilioteca.dominio;
 public class Profesor extends Usuario {
 
     public Profesor(String nombre, String dni, String telefono) {
-        super(nombre, dni, telefono, 5);
+        super(nombre, dni, telefono);
+    }
+
+    @Override
+    public int getMembresia() {
+        return 0;
     }
 }
