c4f020fb44150666e3ecfc38cd7f6111b89a9831
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2024-04-21 11:37:53 +0200

    Create Enum Genero

---
+++ src/bibilioteca/dominio/Genero.java
@@ -0,0 +1,14 @@
+package bibilioteca.dominio;
+
+public enum Genero {
+    FICCION,
+    NOVELA,
+    TERROR,
+    CIENCIA_FICCION,
+    FANTASIA,
+    DRAMA,
+    POESIA,
+    ENSAYO,
+    HISTORIA,
+    OTRO
+}
