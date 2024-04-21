297d5910a831c4ff732edd0aa974bad1cbfa0b0a
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2024-04-21 10:45:47 +0200

    Update Main

--- src/bibilioteca/main/Main.java
+++ src/bibilioteca/main/Main.java
@@ -4,11 +4,13 @@ import bibilioteca.dominio.Alumno;
 import bibilioteca.dominio.Libro;
 import bibilioteca.dominio.Prestamo;
 import bibilioteca.dominio.Profesor;
+import bibilioteca.dominio.Revista;
 
 public class Main {
     public static void main(String[] args) {
         Libro ImperioFinal = new Libro("El Imperio Final", "Brandon Sanderson", "Fantasía", 2006);
         Libro PozoAscension = new Libro("EL Pozo De La Ascensión ", "Brandon Sanderson", "Fantasía", 2007);
+        Revista RoolingStone = new Revista("Rolling Stone", 2024);
 
         /*
          * System.out.println(ImperioFinal.toString());
@@ -21,9 +23,10 @@ public class Main {
         Profesor Xisco = new Profesor("Xisco", "87654321Z", "987 65 43 21");
         System.out.println(Xisco.toString());
 
-        Juan.prestarLibro(ImperioFinal);
-        Juan.prestarLibro(PozoAscension);
-        System.out.println(Juan.getLibros());
+        Juan.prestarArticulo(ImperioFinal);
+        Juan.prestarArticulo(PozoAscension);
+        Juan.prestarArticulo(RoolingStone);
+        System.out.println(Juan.getArticulos());
 
         Prestamo P1 = new Prestamo(ImperioFinal, Juan, "14/04/2024", "14/05/2024");
         System.out.println(P1.getFinal_prestamo());
@@ -32,9 +35,9 @@ public class Main {
         System.out.println(P1.getFinal_prestamo());
 
         System.out.println(P1.toString());
-        P1.regitrar_prestamo();
+        /* P1.regitrar_prestamo();
 
         Prestamo P2 = new Prestamo(PozoAscension, Juan, "17/04/2024", "17/05/2024");
-        P2.regitrar_prestamo();
+        P2.regitrar_prestamo(); */
     }
 }
