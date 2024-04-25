d93d9a3c4fece0bd6fd20a3a3dfaeb6f6b26035a
jesus-dawi <jcaceresfrances@cifpfbmoll.eu>
2024-04-25 16:58:45 +0200

    Update Articulo

--- src/bibilioteca/dominio/Articulo.java
+++ src/bibilioteca/dominio/Articulo.java
@@ -1,8 +1,35 @@
 package bibilioteca.dominio;
 
-public interface Articulo {
+public class Articulo implements Prestable {
+    private final String titulo;
+    private final int año_publicacion;
+    private boolean estado_prestamo;
 
-    void prestar();
+    public Articulo(String titulo, int año_publicacion, boolean estado_prestamo) {
+        this.titulo = titulo;
+        this.año_publicacion = año_publicacion;
+        this.estado_prestamo = estado_prestamo;
+    }
 
-    void devolver();
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getAño_publicacion() {
+        return año_publicacion;
+    }
+
+    public boolean estaPrestado() {
+        return estado_prestamo;
+    }
+
+    @Override
+    public void prestar() {
+        this.estado_prestamo = true;
+    }
+
+    @Override
+    public void devolver() {
+        this.estado_prestamo = false;
+    }
 }
